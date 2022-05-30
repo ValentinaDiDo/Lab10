69f7b2a47e4ab84e1638458f9de0e0c7a895cc24
valentina <valentina@localhost>
2022-05-30 09:04:22 +0200

    Completamento primi metodi

--- src/main/java/it/polito/tdp/rivers/FXMLController.java
+++ src/main/java/it/polito/tdp/rivers/FXMLController.java
@@ -1,13 +1,11 @@
-/**
- * Sample Skeleton for 'Scene.fxml' Controller Class
- */
-
 package it.polito.tdp.rivers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import it.polito.tdp.rivers.model.Model;
+import it.polito.tdp.rivers.model.River;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -15,52 +13,60 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class FXMLController {
-	
 	private Model model;
-
-    @FXML // ResourceBundle that was given to the FXMLLoader
+    @FXML
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML
     private URL location;
 
-    @FXML // fx:id="boxRiver"
-    private ComboBox<?> boxRiver; // Value injected by FXMLLoader
+    @FXML
+    private ComboBox<River> boxRiver;
 
-    @FXML // fx:id="txtStartDate"
-    private TextField txtStartDate; // Value injected by FXMLLoader
+    @FXML
+    private Button btnSimula;
 
-    @FXML // fx:id="txtEndDate"
-    private TextField txtEndDate; // Value injected by FXMLLoader
+    @FXML
+    private TextField txtEndDate;
 
-    @FXML // fx:id="txtNumMeasurements"
-    private TextField txtNumMeasurements; // Value injected by FXMLLoader
+    @FXML
+    private TextField txtFMed;
 
-    @FXML // fx:id="txtFMed"
-    private TextField txtFMed; // Value injected by FXMLLoader
+    @FXML
+    private TextField txtK;
 
-    @FXML // fx:id="txtK"
-    private TextField txtK; // Value injected by FXMLLoader
+    @FXML
+    private TextField txtNumMeasurements;
 
-    @FXML // fx:id="btnSimula"
-    private Button btnSimula; // Value injected by FXMLLoader
+    @FXML
+    private TextArea txtResult;
 
-    @FXML // fx:id="txtResult"
-    private TextArea txtResult; // Value injected by FXMLLoader
+    @FXML
+    private TextField txtStartDate;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
+    @FXML
+    void handleSimula(ActionEvent event) {
+
+    	River river = boxRiver.getValue();
+    	
+    }
+
+    @FXML
     void initialize() {
         assert boxRiver != null : "fx:id=\"boxRiver\" was not injected: check your FXML file 'Scene.fxml'.";
-        assert txtStartDate != null : "fx:id=\"txtStartDate\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnSimula != null : "fx:id=\"btnSimula\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtEndDate != null : "fx:id=\"txtEndDate\" was not injected: check your FXML file 'Scene.fxml'.";
-        assert txtNumMeasurements != null : "fx:id=\"txtNumMeasurements\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtFMed != null : "fx:id=\"txtFMed\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtK != null : "fx:id=\"txtK\" was not injected: check your FXML file 'Scene.fxml'.";
-        assert btnSimula != null : "fx:id=\"btnSimula\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtNumMeasurements != null : "fx:id=\"txtNumMeasurements\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtStartDate != null : "fx:id=\"txtStartDate\" was not injected: check your FXML file 'Scene.fxml'.";
+
     }
-    
-    public void setModel(Model model) {
+
+public void setModel(Model model) {
     	this.model = model;
+    	this.boxRiver.getItems().clear();
+    	this.boxRiver.getItems().addAll(this.model.getAllRivers());
     }
 }
--- src/main/java/it/polito/tdp/rivers/model/Model.java
+++ src/main/java/it/polito/tdp/rivers/model/Model.java
@@ -1,5 +1,19 @@
 package it.polito.tdp.rivers.model;
 
+import java.util.List;
+
+import it.polito.tdp.rivers.db.RiversDAO;
+
 public class Model {
 
+	List<River> rivers;
+	RiversDAO dao;
+	public Model() {
+		this.dao = new RiversDAO();
+	}
+	
+	public List<River> getAllRivers(){
+		this.rivers = dao.getAllRivers();
+		return rivers;
+	}
 }
